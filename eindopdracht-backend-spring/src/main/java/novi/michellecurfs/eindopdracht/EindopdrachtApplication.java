48976bde8f79b2c92cca7c4d30fea1ba943a81e6
smolmies <m.curfs@novi-education.nl>
2021-01-05 21:18:57 +0100

    Backend setup

---
+++ eindopdracht-backend-spring/src/main/java/novi/michellecurfs/eindopdracht/EindopdrachtApplication.java
@@ -0,0 +1,13 @@
+package novi.michellecurfs.eindopdracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EindopdrachtApplication.class, args);
+	}
+
+}
---
+++ eindopdracht-backend-spring/src/test/java/novi/michellecurfs/eindopdracht/EindopdrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package novi.michellecurfs.eindopdracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
